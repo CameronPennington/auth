ad97ff302b78e983b57dbaef3c4c548f02d05c30
CameronPennington <cameron.pennington@gmail.com>
2020-03-11 11:58:38 -0400

    Frontend-authenication
    Able to log in and log out successfully

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { useAuth0 } from "../react-auth0-spa";
 
 const Home = () => {
-	const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
-
-	return (
-		<div>
-			Home
-			<div>
-				<button onClick={() => loginWithRedirect({})}>Log In</button>
-			</div>
-		</div>
-	);
+	return <div>Home</div>;
 };
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,59 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { useAuth0 } from "../react-auth0-spa";
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1
+	},
+	title: {
+		flexGrow: 1
+	}
+}));
 
 const NavBar = () => {
 	const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
-	return <div>NavBar</div>;
+	const classes = useStyles();
+
+	const logoutWithRedirect = () =>
+		logout({
+			returnTo: window.location.origin
+		});
+
+	return (
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6" className={classes.title}>
+						News
+					</Typography>
+
+					{!isAuthenticated && (
+						<Button
+							variant="text"
+							color="inherit"
+							onClick={() => loginWithRedirect({})}
+						>
+							Login
+						</Button>
+					)}
+					{isAuthenticated && (
+						<Button
+							variant="text"
+							color="inherit"
+							onClick={() => logoutWithRedirect()}
+						>
+							Log Out
+						</Button>
+					)}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
 };
+
+export default NavBar;
