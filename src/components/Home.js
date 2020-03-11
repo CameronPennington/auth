285e5107cf28caeec54f56e2c4338bd0da3581df
CameronPennington <cameron.pennington@gmail.com>
2020-03-11 05:15:13 -0400

    Frontend-Authentication
    Login button is linking to Auth0 but receiving an error indicating a misconfiguration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+	const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+	return (
+		<div>
+			Home
+			<div>
+				<button onClick={() => loginWithRedirect({})}>Log In</button>
+			</div>
+		</div>
+	);
+};
 
 export default Home;
